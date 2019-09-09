c6ef8d1a230daa124aa73f6e9c23a9846aa2686b
DimejiAre <olardi2@gmail.com>
2019-09-09 14:02:39 +0100

    add handler function to increment scores using name and amount

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //TODO: STEP 1 - Import the useState hook.
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
@@ -8,6 +8,16 @@ function App() {
 
   const [homeScore, setHomeScore] = useState(0);
   const [awayScore, setAwayScore] = useState(0);
+
+  const updateScore = (name, amount) => {
+    if (name === 'Lions') {
+      setHomeScore(homeScore + amount);
+    } else if (name === 'Tigers') {
+      setAwayScore(awayScore + amount)
+    } else {
+      console.log('invalid name entered')
+    }
+  }
 
   return (
     <div className="container">
@@ -31,12 +41,12 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown" onClick={() => setHomeScore(homeScore + 7)}>Home Touchdown</button>
-          <button className="homeButtons__fieldGoal" onClick={()=> setHomeScore(homeScore + 3)}>Home Field Goal</button>
+          <button className="homeButtons__touchdown" onClick={() => updateScore('Lions', 7)}>Home Touchdown</button>
+          <button className="homeButtons__fieldGoal" onClick={() => updateScore('Lions', 3)}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick={() => setAwayScore(awayScore + 7)}>Away Touchdown</button>
-          <button className="awayButtons__fieldGoal" onClick={()=> setAwayScore(awayScore + 3)}>Away Field Goal</button>
+          <button className="awayButtons__touchdown" onClick={() => updateScore('Tigers', 7)}>Away Touchdown</button>
+          <button className="awayButtons__fieldGoal" onClick={() => updateScore('Tigers', 3)}>Away Field Goal</button>
         </div>
       </section>
     </div>
